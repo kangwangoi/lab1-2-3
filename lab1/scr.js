3eedd725a9099cd0078e54ea561398f67b88a716
kangwangoi <153366806+kangwangoi@users.noreply.github.com>
2024-04-10 23:51:03 +0700

    k
    k

---
+++ lab1/scr.js
@@ -0,0 +1,78 @@
+// const animal = [
+//     {
+//         "name":"cat",
+//         "size":"small",
+//         "weight":5
+//     },
+//     {
+//         "name":"dog",
+//         "size":"small",
+//         "weight":10
+//     },
+//     {
+//         "name":"lion",
+//         "size":"medium",
+//         "weight":150
+//     },
+//     {
+//         "name":"elephant",
+//         "size":"big",
+//         "weight":5000
+//     },
+// ]
+
+// const get = animal.map((i)=>{
+// return i.name})
+// console.log(get)
+
+
+//.................................
+// const num = [1,2,3,4,5,6,7,8,9]
+// const filter = num.filter((i) => {if(i%2==0){
+//     return(i)
+// }})
+// console.log(filer)
+
+
+//............................................
+
+
+// var string = ["cat","dog","hat","mat","java"]
+// const num = [1,2,3,4,5,6,7,8,9]
+// const filter = string.filter((i) => {
+//     return i.includes("at")
+// })
+// console.log(filter)
+
+
+//...........................................
+
+
+// const player = [
+//     {
+//         "name":"ronando",
+//         "team":"mu"
+        
+//     },
+//     {
+//         "name":"messi",
+//         "team":"barca"
+       
+//     },
+//     {
+//         "name":"neymar",
+//         "team":"psg"
+        
+//     },
+  
+// ]
+
+// const get = data=>{
+//     return data.reduce((obj, item) =>
+// {
+//     obj[item.name]= {team:item.team}
+//     return obj
+// },{ })
+// }
+// const playerprof = get(player)
+// console.log(playerprof)
